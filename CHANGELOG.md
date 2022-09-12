d8199a2e630a4cad121b1436916d60871cda6a2e
Duartec <Duartec@IT-WKS017.MidNet.CityOfMiddletown.com>
2022-09-12 15:55:44 -0400

    Increase Version

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+All notable changes to this project will be documented in this file, in reverse chronological order by release.
+
+## 1.0.4
+### Added
+- Sortable and Searchable Tables. 
--- src/Module.php
+++ src/Module.php
@@ -3,6 +3,9 @@ namespace Components;
 
 class Module
 {
+    const TITLE = "Components Module";
+    const VERSION = "v1.0.4";
+    
     public function getConfig()
     {
         return include __DIR__ . '/../config/module.config.php';
