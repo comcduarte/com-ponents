e26c7e77fcdbba3ec1b6939f55ae34be4aac80de
Christopher Duarte <christopher.duarte@middletownct.gov>
2023-10-19 16:14:58 -0400

    Added mapping for navigation and flash messenger partials

--- config/module.config.php
+++ config/module.config.php
@@ -1,5 +1,4 @@
 <?php
-
 use Components\Form\Element\AclDatabaseSelect;
 use Laminas\ServiceManager\Factory\InvokableFactory;
 
@@ -26,15 +25,18 @@ return [
     ],
     'view_manager' => [
         'template_map' => [
-            'base/index' => __DIR__ . '/../view/components/base-views/index.phtml',
-            'base/create' => __DIR__ . '/../view/components/base-views/create.phtml',
-            'base/update' => __DIR__ . '/../view/components/base-views/update.phtml',
-            'base/delete' => __DIR__ . '/../view/components/base-views/delete.phtml',
-            'base/config' => __DIR__ . '/../view/components/base-views/config.phtml',
+            'base/index'    => __DIR__ . '/../view/components/base-views/index.phtml',
+            'base/create'   => __DIR__ . '/../view/components/base-views/create.phtml',
+            'base/update'   => __DIR__ . '/../view/components/base-views/update.phtml',
+            'base/delete'   => __DIR__ . '/../view/components/base-views/delete.phtml',
+            'base/config'   => __DIR__ . '/../view/components/base-views/config.phtml',
             'base/subtable' => __DIR__ . '/../view/components/base-views/subtable.phtml',
-            'base/subform' => __DIR__ . '/../view/components/base-views/subform.phtml',
+            'base/subform'  => __DIR__ . '/../view/components/base-views/subform.phtml',
             
-            'bsFormSelectButton'      => __DIR__ . '/../view/components/bs-form-views/bsFormSelectButton.phtml',
+            'bsFormSelectButton'    => __DIR__ . '/../view/components/bs-form-views/bsFormSelectButton.phtml',
+            
+            'navigation'            => __DIR__ . '/../view/components/partials/navigation.phtml',
+            'flashmessenger'        => __DIR__ . '/../view/components/partials/flashmessenger.phtml',
         ],
         'template_path_stack' => [
             __DIR__ . '/../view',
